39fb239ab97d626e3e8bbe96e22c975091b17f70
Jean Detoeuf <detoeuf.opengroup@aveo-groupe.com>
2014-01-17 15:07:03 +0100

    Il manque le hashcode dans la classe Position

--- src/main/java/org/xteam/goldrush/simu/Position.java
+++ src/main/java/org/xteam/goldrush/simu/Position.java
@@ -27,6 +27,14 @@ public class Position {
 		return ((Position)o).x == x && ((Position)o).y == y;
 	}
 	
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + y;
+        return result;
+    }
+
 	@Override
 	public String toString() {
 		return "(" + x + "," + y + ")";
